872c29282ad4cbbe53531018d6959eece261b0c1
Parham.Roohi <par81hamroohi@gmail.com>
2024-01-23 23:49:04 +0330

    home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,39 @@
-import {Link} from "react-router-dom";
-export default function Home(){
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+export default function Home() {
+  const [tweets, setTweets] = useState([]);
 
-    return <div>
-        <Link to="/tweets">
-        <button style={{background: "plum"}}>Tweets
-        </button> 
-        </Link>
-        <br />
-        <Link to="/users">
-        <button style={{background: "lightblue"}}>Users
-        </button> 
-        </Link>
+  const loadTweets = async () => {
+    const data = await fetch("http://localhost:3000/tweets").then((response) =>
+      response.json()
+    );
+    setTweets(data);
+  };
+
+  useEffect(() => {
+    loadTweets();
+  }, []);
+
+  return <div>
+      <Link to="/tweets">
+        {/* <button style={{background: "plum"}}>
+        Tweets
+        </button>  */}
+        Tweets
+      </Link>
+      <br />
+      <Link to="/users">
+        {/* <button style={{background: "lightblue"}}>Users
+        </button>  */}
+        Users
+      </Link>
+      <div style={{ display: "flex", flexDirection: "column", background: 'red' }}>
+        {tweets.map((t) => (
+          <div key={t.id}>
+            {t.text} {t.createdBy}
+          </div>
+        ))}
+      </div>
     </div>
+  
 }
